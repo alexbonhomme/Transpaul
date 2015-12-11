8f4ba628d18d79fcb56365f343d8844b64aad992
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2015-12-11 23:13:00 +0100

    Implements the 'no bus found meme'

--- js/transpaul.js
+++ js/transpaul.js
@@ -1,5 +1,5 @@
 /*jslint browser:true*/
-/*global transpole, Mustache*/
+/*global transpole, Mustache, moment*/
 
 (function () {
     'use strict';
@@ -10,9 +10,24 @@
         var nexts;
 
         nexts = data.directions[0].nexts.map(function (next) {
-            // todo
-            return next;
+            var nextMoment = moment(next.next);
+            return {
+                next: nextMoment.format('HH:mm'),
+                remaining: nextMoment.subtract(moment()).format('m')
+            };
         });
+
+        // MOCK
+        nexts = [
+            {
+                next: '22:40',
+                remaining: '5'
+            },
+            {
+                next: '22:55',
+                remaining: '15'
+            }
+        ];
 
         return {
             lineName: data.lineName,
@@ -26,6 +41,13 @@
     function handleSuccess(data) {
         var element = document.getElementById('transpaul'),
             template = document.getElementById('template.mst').innerHTML;
+
+        if (!data.directions[0].nexts) {
+            // END OF SERVICE
+            element.innerHTML = '<div class="transpaul-no-bus"><img src="assets/dawson-crying.jpg"><p>Tous les bus sont partis :\'-(</p></div>';
+
+            return;
+        }
 
         element.innerHTML = Mustache.render(template, formatData(data));
     }
