94914d783ed67424ad1f3f2e513e99336ecca6be
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2015-12-11 22:31:38 +0100

    Implements version with correct design using Purecss WIP time

--- js/transpaul.js
+++ js/transpaul.js
@@ -1,15 +1,38 @@
 /*jslint browser:true*/
-/*global transpole, mustache*/
+/*global transpole, Mustache*/
 
 (function () {
     'use strict';
 
-    var transpoleInstance = transpole(),
-        element = document.getElementById('data');
+    var transpoleInstance = transpole();
 
-    transpoleInstance.getNext('18', '773', 'A').then(function (data) {
-        element.innerHTML = data;
-    }, function (error) {
+    function formatData(data) {
+        var nexts;
+
+        nexts = data.directions[0].nexts.map(function (next) {
+            // todo
+            return next;
+        });
+
+        return {
+            lineName: data.lineName,
+            lineMode: data.lineMode.toLowerCase(),
+            stopName: data.stopName,
+            direction: data.directions[0].stopName,
+            nexts: nexts
+        };
+    }
+
+    function handleSuccess(data) {
+        var element = document.getElementById('transpaul'),
+            template = document.getElementById('template.mst').innerHTML;
+
+        element.innerHTML = Mustache.render(template, formatData(data));
+    }
+
+    function handleError(error) {
         console.error(error);
-    });
+    }
+
+    transpoleInstance.getNext('18', '773', 'A').then(handleSuccess, handleError);
 }());
